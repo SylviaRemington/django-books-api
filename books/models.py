4bb07d66f097487871ae0aa97ade4233168c2de6
SylviaRemington <sylviaremington@gmail.com>
2025-10-02 10:45:11 -0700

    addtl notes

--- books/models.py
+++ books/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Book(models.Model):
 #   important
 # need def string and return to actually see the data
+# This is string data that helps with working with your admin area on localhost:8000/admin
   def __str__(self):
     return f'{self.title} - a book by {self.author}'
   title = models.CharField(max_length=80, unique=True)
   author = models.CharField(max_length=50)
   genre = models.CharField(max_length=60)
   year = models.FloatField()
-  
