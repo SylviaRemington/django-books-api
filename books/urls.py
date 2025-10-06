f0801e9df473c48e12266841bdc4ef6c346c6e1e
SylviaRemington <sylviaremington@gmail.com>
2025-10-05 17:57:37 -0700

    adding to the books urls with the integer pk id path

--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,9 @@ from .views import BookListView, BookDetailView
 # URLS.PY IS FOR THE PATHS - urls deal with the views
 urlpatterns = [
   path('', BookListView.as_view()),
+
+  # This below is Django's way of doing :id, and uses '<int:pk/' instead.
+  # It expects it to have an integer which is called the pk.
   path('<int:pk/', BookDetailView.as_view()),
 ]
+
