5f7805e707d7782eba42457f83915e1132605dd9
SylviaRemington <sylviaremington@gmail.com>
2025-10-05 18:00:44 -0700

    addtl notes

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,7 @@ from .views import BookListView, BookDetailView
 
 # URLS.PY IS FOR THE PATHS - urls deal with the views
 urlpatterns = [
+  # If it's just '/books' then it will be this view; however if it has an id, it will be the next path below.
   path('', BookListView.as_view()),
 
   # This below is Django's way of doing :id, and uses '<int:pk/' instead.
@@ -10,5 +11,4 @@ urlpatterns = [
   # Don't need to also add this path to the project urls.py because this falls under the books urls.
   path('<int:pk/', BookDetailView.as_view()),
 ]
-
 
