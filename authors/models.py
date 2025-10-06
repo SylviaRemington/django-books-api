2b37185c2f452ef2608d8fec5f171690380e3b17
SylviaRemington <sylviaremington@gmail.com>
2025-10-06 00:05:37 -0700

    created authors model

--- authors/models.py
+++ authors/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 # Create your models here.
-# class Author(models.Model): -- need to complete
+class Author(models.Model):
+    def __str__(self):
+        return f'{self.name}'
+
+    name = models.TextField(max_length=300)
