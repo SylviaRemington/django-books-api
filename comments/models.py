c6ed94d54e2a41b62dfb50118ac3bc99f4d5425f
SylviaRemington <sylviaremington@gmail.com>
2025-10-06 01:47:17 -0700

    adding comments app with a model a serializer and registering it in admin and in projects settings.py

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    def __str__(self):
+        return f'{self.text} - {self.book}'
+    text = models.TextField(max_length=300)
+    # auto now true means that when a comment is created it will add a timestamp
+    # This will automatically set the created_at field to the current date and time when the comment is created.
+    created_at = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(
+        "books.Book",
+        related_name = "comments",
+        on_delete=models.CASCADE
+    )
