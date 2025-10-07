d8b0fc8129b9a0f17b3febf45711afd8ed1cae07
SylviaRemington <sylviaremington@gmail.com>
2025-10-07 16:00:46 -0700

    addtl in admin.py and models.py in jwt

--- jwt_auth/admin.py
+++ jwt_auth/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+# links through the project settings (get user model) to deliver the model
+# So if we change what is in the project model, it will also change what is delivered here.
 from django.contrib.auth import get_user_model #get the user model from project settings
 
 # Register your models here
--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,7 +1,9 @@
+# This line 2 already has pwd confirmation, pwd, username as keys on that model from Django out of the box
 from django.db import models
 from django.contrib.auth.models import AbstractUser # user model that already exists in django
 
 # Models here:
+# This is inheriting, because we are extending the AbstractUser.
 class User(AbstractUser): # we extend the AbstractUser and add the fields that we want for our users
     email = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=50)
